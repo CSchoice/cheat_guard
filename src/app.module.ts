55b031adddd8e85819f734fd27adb39988d07338
CSchoice <tkdlqjf@gmail.com>
2025-06-16 14:55:48 +0900

    feat : 기본 모듈 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+  ],
 })
 export class AppModule {}
