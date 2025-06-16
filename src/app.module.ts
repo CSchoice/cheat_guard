5be3fafede39ea01d59c0ba09a4b9a2b6d008f74
CSchoice <tkdlqjf@gmail.com>
2025-06-16 14:58:55 +0900

    feat : DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from './configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mariadb',
+        host: config.get<string>('db.host'),
+        port: config.get<number>('db.port'),
+        username: config.get<string>('db.username'),
+        password: config.get<string>('db.password'),
+        database: config.get<string>('db.database'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        extra: {
+          charset: 'utf8mb4_general_ci',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+
   ],
 })
 export class AppModule {}
